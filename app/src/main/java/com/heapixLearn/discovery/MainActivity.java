ff8383e81d0ca959de3b67cfb227370a7b381bfb
Agent00krosh <igor201012015@gmail.com>
2019-01-25 14:11:20 +0200

    commit with test

--- app/src/main/java/com/heapixLearn/discovery/MainActivity.java
+++ app/src/main/java/com/heapixLearn/discovery/MainActivity.java
@@ -19,6 +19,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RunnableWithObject<List<ServerContact>> onSuccessList = new RunnableWithObject<List<ServerContact>>(){
+            @Override
+            public void run(){
+
+            }
+        };
+
+        RunnableWithObject<TypeOfServerError> onFailure = new RunnableWithObject<TypeOfServerError>(){
+            @Override
+            public void run(){
+                Toast.makeText(AppContext.getInstance(), this.getDescription().getTypeOfServerError(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        RunnableWithObject<ServerContact> onSuccessContact = new RunnableWithObject<ServerContact>(){
+            @Override
+            public void run(){
+
+            }
+        };
+
+        Runnable onSuccess = new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(AppContext.getInstance(), "Success", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        List<ServerContact> serverContact= new ArrayList<ServerContact>();
+
+        ServerContact contact = new ServerContact();
+        contact.setId("1");
+        contact.setName("qwerty");
+        contact.setEmail("ins@kdmcm.com");
+
+        serverContact.add(contact);
+
+        ServerContactManager serverContactManager = new ServerContactManager();
+        serverContactManager.createContact(contact, onSuccess, onFailure);
+        serverContactManager.getContactById("1", onSuccessContact, onFailure);
+        serverContactManager.updateContact(contact, onSuccess, onFailure);
+        serverContactManager.deleteContact(contact, onSuccess, onFailure);
+        serverContactManager.readContacts(onSuccessList, onFailure);
 
     }
 }
