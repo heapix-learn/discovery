dc1525223b795f36d2c66edf73be392ee9083cf8
agent00krosh <igor201012015@gmail.com>
2019-01-29 23:41:21 +0200

    added methods for update

--- app/src/main/java/com/heapixLearn/discovery/ui/news/NewsFragment.java
+++ app/src/main/java/com/heapixLearn/discovery/ui/news/NewsFragment.java
@@ -10,28 +10,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.heapixLearn.discovery.help.NewsManager;
+
+
 import com.heapixLearn.discovery.R;
+
+import java.util.List;
 
 public class NewsFragment extends Fragment {
 
     private View newsFragment;
-    private NewsManager newsManager = new NewsManager();
+
+    private NewsManager newsManager;
+    private RecyclerView recyclerView;
+    private NewsAdapter adapter;
+    private List<NewsItem> newsItems;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         newsFragment = inflater.inflate(R.layout.news_fragment, container, false);
+
         initPosts();
         return newsFragment;
     }
 
     private void initPosts(){
-        RecyclerView recyclerView = (RecyclerView) newsFragment.findViewById(R.id.news_recycle_view);
-        NewsAdapter adapter = new NewsAdapter(newsManager.getAll(), getContext());
+        //TODO initialization newsManager
+        recyclerView = (RecyclerView) newsFragment.findViewById(R.id.news_recycle_view);
+        newsItems = newsManager.getAll();
+        adapter = new NewsAdapter(newsItems, getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    public void updateAll(List<NewsItem> news){
+        newsItems.clear();
+        newsItems.addAll(news);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void updateNewsItem(NewsItem oldNewsItem, NewsItem newNewsItem){
+        newsItems.remove(oldNewsItem);
+        newsItems.add(newNewsItem);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/heapixLearn/discovery/ui/news/NewsManager.java
@@ -0,0 +1,7 @@
+package com.heapixLearn.discovery.ui.news;
+
+import java.util.List;
+
+public interface NewsManager {
+    List<NewsItem> getAll();
+}
