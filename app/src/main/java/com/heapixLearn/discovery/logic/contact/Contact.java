4bef4679a825f7294203cd9c56cade0a9ff2c078
ViachaslauPratasenia <slavaprot14@gmail.com>
2019-01-31 23:43:21 +0300

    Create Contact.java

---
+++ app/src/main/java/com/heapixLearn/discovery/logic/contact/Contact.java
@@ -0,0 +1,47 @@
+package com.heapixLearn.discovery.logic.contact;
+
+public class Contact implements com.heapixLearn.discovery.ui.contact.Contact {
+
+    private int id;
+    private String name;
+    private int followers;
+    private String avatar;
+    private boolean isFriend;
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getFollowers() {
+        return followers;
+    }
+
+    @Override
+    public String getAvatar() {
+        return avatar;
+    }
+
+    @Override
+    public boolean isFriend() {
+        return isFriend;
+    }
+
+    @Override
+    public void setFriend(boolean friend) {
+        isFriend = friend;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (!(o instanceof Contact)) return false;
+        Contact document = (Contact) o;
+        return id == document.getId();
+    }
+}
