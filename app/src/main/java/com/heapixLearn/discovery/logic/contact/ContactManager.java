846beaab7e866e5b12ff38f68fda68d1449a5ddb
ViachaslauPratasenia <slavaprot14@gmail.com>
2019-02-01 15:22:26 +0300

    commit without contact test

--- app/src/main/java/com/heapixLearn/discovery/logic/contact/ContactManager.java
+++ app/src/main/java/com/heapixLearn/discovery/logic/contact/ContactManager.java
@@ -17,22 +17,11 @@ public class ContactManager implements com.heapixLearn.discovery.ui.contact.Cont
 
     @Override
     public Contact getById(int id) {
-        ArrayList<Contact> contacts = getAll();
-        for(Contact contact : contacts){
-            if(contact.getId() == id){
-                return contact;
-            }
-        }
         return null;
     }
 
     @Override
     public ArrayList<Contact> getAll() {
-        ArrayList<Contact> contacts = new ArrayList<>();
-        contacts.add(new com.heapixLearn.discovery.logic.contact.Contact(1, "TestName",
-                100, "avatar", true));
-        contacts.add(new com.heapixLearn.discovery.logic.contact.Contact(2, "TestName",
-                100, "avatar", true));
-        return contacts;
+        return null;
     }
 }
