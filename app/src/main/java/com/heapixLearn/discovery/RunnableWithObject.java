b06772831a63bb142d4a0670d9cadf62c3cfc6a1
ViachaslauPratasenia <slavaprot14@gmail.com>
2019-02-22 13:54:52 +0300

    Corrected RunnableWithObject

--- app/src/main/java/com/heapixLearn/discovery/RunnableWithObject.java
+++ app/src/main/java/com/heapixLearn/discovery/RunnableWithObject.java
@@ -1,15 +1,11 @@
 package com.heapixLearn.discovery;
 
-public class RunnableWithObject<T> implements Runnable {
+public abstract class RunnableWithObject<T> implements Runnable {
     private T object;
     public void init(T object){
         this.object = object;
     }
     public T getObject() {
         return object;
-    }
-
-    @Override
-    public void run() {
     }
 }
